e751628b5e6026f6301b5d7ef6dc72ffdaac253b
Bernardo Heynemann <heynemann@gmail.com>
2014-08-12 10:19:19 -0300

    virtualenv function identation

--- fish/functions/get_virtualenv.fish
+++ fish/functions/get_virtualenv.fish
@@ -1,5 +1,5 @@
 function get_virtualenv --description="Gets the current virtualenv and interpreter version" 
-	test -n "$VIRTUAL_ENV"; or return
+    test -n "$VIRTUAL_ENV"; or return
     set -l pythonv (python -c 'import sys; print (".".join([str(item) for item in sys.version_info[:3]]))')
     set_color ff9000
     printf "+%s" (basename $VIRTUAL_ENV)
