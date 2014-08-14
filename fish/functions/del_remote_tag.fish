4073b0cbdfe2bdd0169be43c981976cb70c1a99f
Bernardo Heynemann <heynemann@gmail.com>
2014-08-14 10:38:20 -0300

    del remote tag and branch

--- fish/functions/del_remote_tag.fish
+++ fish/functions/del_remote_tag.fish
@@ -1,4 +1,4 @@
-function --description="Deletes the remote tag specified in $argv" del_remote_tag
-	test -n "$argv"; or return
-    git push origin :refs/tags/$argv
+function --description="Deletes the remote tag specified in argv" del_remote_tag
+        test -n "$argv"; or return
+        git push origin :refs/tags/$argv
 end
