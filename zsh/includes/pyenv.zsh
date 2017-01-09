27ee7e80753ba154a9994b1e55a94e17a961b195
Bernardo Heynemann <heynemann@gmail.com>
2017-01-09 00:37:29 -0200

    Pyenv

--- zsh/includes/pyenv.zsh
+++ zsh/includes/pyenv.zsh
@@ -1,4 +1,5 @@
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
+#eval "$(pyenv virtualenv-init -)"
+export VIRTUAL_ENV_DISABLE_PROMPT=1
