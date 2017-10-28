c3241eccdff064248b1f8e63ebde70a7f1a8c9ae
Bernardo Heynemann <heynemann@gmail.com>
2017-10-28 00:24:55 -0200

    bundles and last bundles

--- zsh/includes/bundles.zsh
+++ zsh/includes/bundles.zsh
@@ -1,4 +1,4 @@
-source /Users/heynemann/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions/zsh-completions.plugin.zsh
-source /Users/heynemann/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-MichaelAquilina-SLASH-zsh-autoswitch-virtualenv/autoswitch_virtualenv.plugin.zsh
-source /Users/heynemann/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-bhilburn-SLASH-powerlevel9k/powerlevel9k.zsh-theme
-source /Users/heynemann/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
+source /home/heynemann/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions/zsh-completions.plugin.zsh
+source /home/heynemann/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-MichaelAquilina-SLASH-zsh-autoswitch-virtualenv/autoswitch_virtualenv.plugin.zsh
+source /home/heynemann/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-bhilburn-SLASH-powerlevel9k/powerlevel9k.zsh-theme
+source /home/heynemann/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
