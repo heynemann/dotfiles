d807a54a5b85354b1e8a071cc3874a236f47c5fa
Bernardo Heynemann <heynemann@gmail.com>
2017-10-28 00:25:27 -0200

    remove bundles and last bundles

--- zsh/includes/bundles.zsh
+++
@@ -1,4 +0,0 @@
-source /home/heynemann/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions/zsh-completions.plugin.zsh
-source /home/heynemann/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-MichaelAquilina-SLASH-zsh-autoswitch-virtualenv/autoswitch_virtualenv.plugin.zsh
-source /home/heynemann/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-bhilburn-SLASH-powerlevel9k/powerlevel9k.zsh-theme
-source /home/heynemann/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
--- zsh/includes/last-bundles.zsh
+++
@@ -1,2 +0,0 @@
-source /home/heynemann/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search/zsh-history-substring-search.zsh
-source /home/heynemann/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
