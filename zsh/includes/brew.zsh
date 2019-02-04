091c91c8f44b12f1cc342530b7daa4305e4ae515
Bernardo Heynemann <heynemann@gmail.com>
2019-02-04 11:14:04 -0200

    Linuxbrew

--- zsh/includes/brew.zsh
+++ zsh/includes/brew.zsh
@@ -4,6 +4,7 @@ brew() {
     brew $@
 }
 
-
-export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
-eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+if [ -f "/home/linuxbrew/.linuxbrew/bin/brew" ]; then
+    export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
+    eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+fi
