69c6b75db5615fb4e39c08f9c971dbbf82534793
Bernardo Heynemann <heynemann@gmail.com>
2020-04-10 21:43:18 +0100

    ZSH Bind Keys

--- zsh/includes/general.zsh
+++ zsh/includes/general.zsh
@@ -138,3 +138,36 @@ export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
 TIPZ_TEXT='💡 '
 export VISUAL=vim
 export EDITOR="$VISUAL"
+
+# If NumLock is off, translate keys to make them appear the same as with NumLock on.
+bindkey -s '^[OM' '^M'  # enter
+bindkey -s '^[Ok' '+'
+bindkey -s '^[Om' '-'
+bindkey -s '^[Oj' '*'
+bindkey -s '^[Oo' '/'
+bindkey -s '^[OX' '='
+#
+# # If someone switches our terminal to application mode (smkx), translate keys to make
+# # them appear the same as in raw mode (rmkx).
+bindkey -s '^[OH' '^[[H'  # home
+bindkey -s '^[OF' '^[[F'  # end
+bindkey -s '^[OA' '^[[A'  # up
+bindkey -s '^[OB' '^[[B'  # down
+bindkey -s '^[OD' '^[[D'  # left
+bindkey -s '^[OC' '^[[C'  # right
+#
+# # TTY sends different key codes. Translate them to regular.
+bindkey -s '^[[1~' '^[[H'  # home
+bindkey -s '^[[4~' '^[[F'  # end
+
+# ZSH Navigation
+# Word Navigation
+bindkey "^[[1;5D" backward-word
+bindkey "^[[1;5C" forward-word
+
+# Home/End
+bindkey "^[[H" beginning-of-line
+bindkey "^[[F" end-of-line
+
+# Del
+bindkey "^[[3~" delete-char
