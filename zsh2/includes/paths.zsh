fe1687da0712f53ec76531ce11d3ac36ce4325fb
Bernardo Heynemann <heynemann@gmail.com>
2021-09-04 18:49:30 -0300

    Basic stuff on new ZSH

---
+++ zsh2/includes/paths.zsh
@@ -0,0 +1,5 @@
+# Linux Brew
+PATH=$PATH:/home/linuxbrew/.linuxbrew/bin
+
+# Home folder binaries
+PATH=$PATH:~/bin
---
+++ zsh2/includes/pure.zsh
@@ -0,0 +1,6 @@
+# Pretty, minimal and fast ZSH prompt
+# https://github.com/sindresorhus/pure
+# Install with 'brew install pure'
+
+[[ ( -f /home/linuxbrew/.linuxbrew/share/zsh/site-functions/prompt_pure_setup ) ]] && autoload -U promptinit; promptinit && prompt pure
+[[ ( ! -f /home/linuxbrew/.linuxbrew/share/zsh/site-functions/prompt_pure_setup ) ]] && print Pure was not found. Please install with \'brew install pure\'.
